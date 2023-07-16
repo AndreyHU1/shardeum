66c97f8533bc0b725e0331dea1a499e48a541d4e
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2023-07-17 02:56:04 +0600

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/root/bin/bash
+channel_id=$(awk '/shardeum_channel_id:/ {print $NF}' config);
+tg_bot_token=$(awk '/tg_bot_token:/ {print $NF}' config);
+node_name=$(awk '/node_name:/ {print $NF}' config);
+docker exec -t shardeum-dashboard operator-cli status > status.txt
+version=$(awk '/shardeumVersion:/ {print $NF}' status.txt)
+if [[ $version != *".5.1"* ]]; then
+	sudo curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+	sudo expect update.exp
+	docker exec -t shardeum-dashboard operator-cli start
+	sleep 20
+	docker exec -t shardeum-dashboard operator-cli status > status.txt
+	version=$(awk '/shardeumVersion:/ {print $NF}' status.txt)
+	curl -F chat_id="$channel_id" -F text="⭐️⭐️⭐️ $node_name: updated to version 🌵 $version 🌵 ⭐️⭐️⭐️" https://api.telegram.org/bot"$tg_bot_token"/sendMessage
+fi 

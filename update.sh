a5e64ac8f8a775b5ad557bfdcdb3febc4d983bf7
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2023-07-22 03:04:28 +0600

    Update update.sh
    • add status.sh to update list

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ tg_bot_token=$(awk '/tg_bot_token:/ {print $NF}' config);
 node_name=$(awk '/node_name:/ {print $NF}' config);
 docker exec -t shardeum-dashboard operator-cli status > status.txt
 version=$(awk '/shardeumVersion:/ {print $NF}' status.txt)
+sudo curl -O https://raw.githubusercontent.com/AndreyHU1/shardeum/main/status.sh
 if [[ $version == *".5.1"* ]]; then
 	sudo curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
  	sudo curl -O https://raw.githubusercontent.com/AndreyHU1/shardeum/main/update.exp && chmod +x update.exp

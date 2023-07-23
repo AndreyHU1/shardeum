8783d65ce55156fff77a0000f86c7c92f67f4839
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2023-07-23 23:23:48 +0600

    Update status.sh

--- status.sh
+++ status.sh
@@ -20,34 +20,35 @@ echo "Earn: $earn";
 echo "Version: $version";
 
 for (( i=0; i<5; i++)); do
+	echo "$i"
+	if ((i>0)); then 
+		echo "sleep 10sec";
+		sleep 30;
+		docker exec -t shardeum-dashboard operator-cli status > status.txt
+		status=$(awk '/state:/ {print $NF}' status.txt | sed 's/.$//');
+		staked=$(awk '/lockedStake:/ {print $NF}' status.txt);
+		earn=$(awk '/currentRewards:/ {print $NF}' status.txt);
+	fi
+	
 	if [[ $status == *"tandb"* ]]; then 
 		curl -F chat_id="$channel_id" -F text="✅ $node_name: $status | Staked: $staked | Earn: $earn | Ver: $version" https://api.telegram.org/bot"$tg_bot_token"/sendMessage;
 		if [[ $staked == "'0.0"* ]]; then # тут нужно проверять, как будет работать неэкранированная кавычка
-			sleep 10;
 			echo "zero staking"
-			docker exec -it shardeum-dashboard operator-cli status > /root/status.txt;
-			staked=$(awk '/lockedStake:/ {print $NF}' status.txt);
 		else
 			i=10;
 		fi
 	else
 		if [[ $status == *"ctiv"* ]]; then
 			curl -F chat_id="$channel_id" -F text="⭐️ $node_name: $status | Staked: $staked | Earn: $earn | Ver: $version" https://api.telegram.org/bot"$tg_bot_token"/sendMessage;
-			sleep 60;
+			sleep 120;
 			i=0;
-			docker exec -it shardeum-dashboard operator-cli status > /root/status.txt;
-			status=$(awk '/state:/ {print $NF}' status.txt);
 		else	
 			if [[ $status == *"toppe"* ]]; then 
 				curl -F chat_id="$channel_id" -F text="⛔️ $node_name: $status | Staked: $staked | Earn: $earn | Ver: $version" https://api.telegram.org/bot"$tg_bot_token"/sendMessage;
-				docker exec -it shardeum-dashboard operator-cli start;
-				sleep 10;
-				docker exec -it shardeum-dashboard operator-cli status > /root/status.txt
-				status=$(awk '/state:/ {print $NF}' status.txt);
-				version=$(awk '/shardeumVersion:/ {print $NF}' status.txt);
+				docker exec -t shardeum-dashboard operator-cli start;
+				sleep 20;
 			else
 				curl -F chat_id="$channel_id" -F text="❌ $node_name: $status | Staked: $staked | Earn: $earn | Ver: $version" https://api.telegram.org/bot"$tg_bot_token"/sendMessage;
-				i=10;
 			fi
 		fi
 	fi

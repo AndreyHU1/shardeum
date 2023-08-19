d5f210d1a8d8214f2c89e44f1976209fa4f5b4ab
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2023-08-19 14:00:06 +0600

    Update start.sh
    add apt update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt update
 sudo apt install docker.io -y
 sudo apt install expect -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose

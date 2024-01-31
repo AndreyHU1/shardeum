d251c866ea04c5563dd512087652c763eb4c6ff2
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2024-01-31 11:33:00 +0600

    Create start_temp.sh

---
+++ start_temp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+curl -O https://github.com/AndreyHU1/shardeum/blob/26c708ebcfcfb27db5f45ae8d3c089f835f4db04/auto_temp.exp && chmod +x auto_temp.exp
+sudo expect auto_temp.exp
+sudo docker exec shardeum-dashboard operator-cli start
+sleep 5
+sudo bash status.sh #sudo ./status.sh

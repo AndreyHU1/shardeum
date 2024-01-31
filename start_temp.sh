d74267c74213b53faaea49b456b999b673f18d92
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2024-01-31 12:05:01 +0600

    Update start_temp.sh

--- start_temp.sh
+++ start_temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O https://github.com/AndreyHU1/shardeum/blob/26c708ebcfcfb27db5f45ae8d3c089f835f4db04/auto_temp.exp && chmod +x auto_temp.exp
+curl -O https://raw.githubusercontent.com/AndreyHU1/shardeum/main/auto_temp.exp && chmod +x auto_temp.exp
 sudo expect auto_temp.exp
 sudo docker exec shardeum-dashboard operator-cli start
 sleep 5

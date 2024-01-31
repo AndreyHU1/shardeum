7bc4f2de9a8a2e2d327235aa5ca0ef020b14f50e
AndreyHU1 <107914634+AndreyHU1@users.noreply.github.com>
2024-01-31 11:43:52 +0600

    Update start_temp.sh
    no installer update

--- start_temp.sh
+++ start_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh
+# curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh
 curl -O https://github.com/AndreyHU1/shardeum/blob/26c708ebcfcfb27db5f45ae8d3c089f835f4db04/auto_temp.exp && chmod +x auto_temp.exp
 sudo expect auto_temp.exp
 sudo docker exec shardeum-dashboard operator-cli start
